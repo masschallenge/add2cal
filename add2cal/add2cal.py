fb004de70a760b385baed069cf2478ca1ce9e5b0
Amos Welike <amoswelike@gmail.com>
2023-09-14 22:30:06 +0300

    update date format

--- add2cal/add2cal.py
+++ add2cal/add2cal.py
@@ -18,7 +18,8 @@ BASE_URLS = {
 
 INPUT_DATE_FORMAT = "%Y%m%dT%H%M%S"
 # OUTLOOK_DATE_FORMAT = '%Y-%m-%dT%I:%M:%SZ'
-OUTLOOK_DATE_FORMAT = 'yyyy-mm-ddThh:mm:ssZ'
+# OUTLOOK_DATE_FORMAT = 'yyyy-mm-ddThh:mm:ssZ'
+OUTLOOK_DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 TRIGGER_DATE_FORMAT = '%Y-%m-%dT%I:%M'
 
 
@@ -106,13 +107,13 @@ class Add2Cal():
         return _build_url(BASE_URLS['yahoo'], params)
 
     def outlook_calendar_url(self):
-        end = datetime.strptime(self.end_datetime, OUTLOOK_DATE_FORMAT)
-        start = datetime.strptime(self.start_datetime, OUTLOOK_DATE_FORMAT)
+        end = datetime.strptime(self.end_datetime, INPUT_DATE_FORMAT)
+        start = datetime.strptime(self.start_datetime, INPUT_DATE_FORMAT)
 
         params = {
             'rru': 'addevent',
-            'startdt': start,
-            'enddt': end,
+            'startdt': start.strftime(OUTLOOK_DATE_FORMAT),
+            'enddt': end.strftime(OUTLOOK_DATE_FORMAT),
             'subject': self.event_title,
             # 'uid': self.event_uid,
             'location': self.event_location,
